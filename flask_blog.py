a54e0662ca195b049c9545d577284f9f69c7e472
gboyega.a@credpal.com <gboyega.a@credpal.com>
2020-02-14 09:52:12 +0100

    initial commit

---
+++ flask_blog.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd29e808cb25a8d4cc3f71ece460c433d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flaskblog/routes.py
@@ -0,0 +1,76 @@
+from flaskblog.models import User, Post
+from flaskblog.forms import RegistrationForm, LoginForm
+from flask import Flask, render_template, url_for, flash, redirect, request
+from flaskblog import app, db, bcrypt
+from flask_login import login_user, logout_user, current_user, login_required
+
+
+posts = [
+    {
+        'title':'the first post',
+        'author':'the owner',
+        'content': ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'post_date': 'Jan. 27, 2020'
+    },
+
+    {
+        'title':'the second post',
+        'author':'its owner',
+        'content': ' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'post_date': 'Jan. 27, 2021'
+    }
+]
+
+@app.route('/')
+@app.route('/index')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about/')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/register/',methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        user = User(username=username,email=email,password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f'Account created successfully for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, password):
+                login_user(user, remember=form.remember.data)
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('home'))
+            else:
+                flash('Login unsuccessful. wrong password', 'danger')
+        else:
+            flash('Login unsuccessful. invalid email', 'danger')
+
+    return render_template('login.html', title='Log In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
